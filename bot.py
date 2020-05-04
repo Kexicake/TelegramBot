8bf58f454ca30205e6e11a10770a3af6320a61b2
vladandlexa <slva.kuznetsov.2017@gmail.com>
2020-05-04 17:51:24 +0400

    0.3

--- bot.py
+++ bot.py
@@ -14,37 +14,51 @@ keyboard1.row('/help')
 
 url = 'https://translate.yandex.net/api/v1.5/tr.json/translate?' 
 key = 'trnsl.1.1.20200420T153840Z.c64aa64b1ef12707.339289160fc33c01cc0c66ce2604c7200714bf48' 
-lang = 'en-ru','ru-en' 
+words = '' 
+langs = ['ru-en','ru-de' ,'ru-tt' ,'ru-uk']
+langs_name = ['на английский', 'на немецкий', 'на татарский', 'на украинский']
 
 _reversB = False
-_translateB = False
 _opredelenieB = False
 
+def log(message):
+    history = open('history.txt', 'a')
+    history.write( '@' + str(message.chat.username) + ' ' + str(message.chat.id) + ' ' + str(message.from_user.first_name) + ' ' + str(message.content_type) + ' - ' + str(message.text) + ' ' + time.ctime() + '\n')
+    history.close()
 
+def revers(text):
+    message_revers = ''
+    i = len(text)
+    while i > 0:
+        i -= 1 
+        message_revers += text[i]
+    return message_revers
 
 @bot.message_handler(commands=['start'])
 def start_message(message):
+    log(message)
     f = open('start.txt', 'r')
-    history = open('history.txt', 'a')
-    history.write( '@' + str(message.chat.username) + ' ' + str(message.chat.id) + ' ' + time.ctime() + '\n')
     bot.send_message(message.chat.id,  f.read(), reply_markup=keyboard1)
     f.close()
-    history.close()
+
 
 @bot.message_handler(commands=['help'])
 def help_message(message):
+    log(message)
     f = open('help.txt', 'r')
     bot.send_message(message.chat.id,  f.read(), reply_markup=keyboard_help)
     f.close()
 
 @bot.message_handler(commands=['version'])
 def version_message(message):
+    log(message)
     f = open('version.txt', 'r')
     bot.send_message(message.chat.id,  f.read(), reply_markup=keyboard_help)
     f.close()
 
 @bot.message_handler(commands=['revers'])
 def revers_message(message):
+    log(message)
     global _reversB
     if _reversB == False:
         _reversB = True
@@ -53,69 +67,78 @@ def revers_message(message):
         _reversB = False
         bot.send_message(message.chat.id, 'revers off')
 
-@bot.message_handler(commands=['translate'])
-def translate_message(message):
-    global _translateB
-    if _translateB == False:
-        _translateB = True
-        bot.send_message(message.chat.id, 'translate on')
-    else:
-        _translateB = False
-        bot.send_message(message.chat.id, 'translate off')
-
 @bot.message_handler(commands=['opr'])
 def opr_message(message):
+    log(message)
     global _opredelenieB
     if _opredelenieB == False:
         _opredelenieB = True
         bot.send_message(message.chat.id, 'Dicshinory on')
     else:
         _opredelenieB = False
-        bot.send_message(message.chat.id, 'Dicshinoryoff')
+        bot.send_message(message.chat.id, 'Dicshinory off')
 
 @bot.message_handler(content_types=['text'])
 def send_text(message): 
-    global _reversB, _translateB
+    global _reversB, words
+    words = message
+    log(message)
     if _reversB == True:
-        message_revers = ''
-        i = len(message.text)
-        while i > 0:
-            i -= 1 
-            message_revers += message.text[i] 
-        bot.send_message(message.chat.id, message_revers)
-    elif  _translateB == True:
-        global url, key, lang
-        translated = requests.post(url, data={'key': key, 'text': message.text, 'lang': lang})
-        not_farm_text = translated.text
-        sb = False
-        word = ''
-        i = 0
-        while i<len(not_farm_text):
-            if not_farm_text[i-2] == '[':
-                sb = True
-            if not_farm_text[i] == '"':
-                sb = False
-            if sb == True:
-                word += not_farm_text[i]
-            i += 1
-        text = 'Слово/a ' + message.text + ' на английском - ' + word
-        bot.send_message(message.chat.id, text)
+        bot.send_message(message.chat.id, revers(message.text))
+    elif  message.text[:8] == 'Переведи':
+        global url, key, langs, langs_name
+        message_revers = revers(message.text)
+        endword = 0
+        count = 0
+        indicator = False
+
+        while indicator == False:
+            if message_revers[endword] == ' ':
+                count += 1
+            if count == 2:
+                indicator = True
+            endword += 1
+        endword -= 1
+        text_lang = revers(message_revers[:endword])
+
+
+        ir = 0
+        lang = ''
+        ind = False
+        while ir <= len(langs_name):
+            if ir == len(langs_name):
+                bot.send_message(message.chat.id, 'Такого языка ещё нет в базе или он не существует!')
+                ind = True
+                break
+            if langs_name[ir] == text_lang:
+                lang = langs[ir]
+            ir += 1 
+        if ind == False:
+            text = message.text[9:(len(message.text) - endword)] 
+            #lang = "ru-en"
+            reque = requests.get(url, data={'key': key, 'text': text, 'lang': lang})
+            #text = text + ' - ' + word
+            translated = reque.json()
+            bot.send_message(message.chat.id, translated["text"])
+
+            #msg = bot.send_message(message.chat.id, 'На какой язык перевести?')
+            #bot.register_next_step_handler(msg, translate) 
     elif _opredelenieB == True:
         wikipedia.set_lang("ru")
-        bot.send_message(message.chat.id, wikipedia.summary(message.text, sentences=2))
-    elif message.text == 'Пока':
-        bot.send_message(message.chat.id, 'Пака')
-    elif message.text == 'Привет':
-        bot.send_message(message.chat.id, 'Привет, мой создатель')    
+        bot.send_message(message.chat.id, wikipedia.summary(message.text, sentences = 2))   
     else:
         bot.send_message(message.chat.id, 'Я тебя не понимаю(')
+
     
 @bot.message_handler(content_types=['sticker'])
 def sticker_id(message):
+    log(message)
     bot.send_message(message.chat.id, message)
 
 @bot.message_handler(content_types=['audio'])
 def handle_audio(message):
-	bot.send_message(message.chat.id, 'Норм музочка)')
+    log(message)
+    bot.send_message(message.chat.id,'Норм музочка)')
+
 
 bot.polling()
