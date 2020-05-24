edabd6d2dda0fa672aa7e7150fc3ee751dbce891
vladandlexa <slava.kuznetsov.2017@gmail.com>
2020-05-24 14:33:14 +0300

    Update

--- tel_bot.py
+++ tel_bot.py
@@ -4,8 +4,10 @@ class Tel_bot:
         """Constructor"""
         self.bot = telebot.TeleBot(token)
         print('Construction complit!\n-----------------------------\n')
+
     def sendMes(self, message, text):
         self.bot.send_message(message.chat.id, text)
+
     def command(self, message):
         keyboard_help = telebot.types.ReplyKeyboardMarkup(True)
         keyboard_help.row('/start', '/help', '/revers', '/translate')
@@ -29,12 +31,13 @@ class Tel_bot:
             text = 'bad'
             self.sendMes(message, text)
         f.close()
+
     def translate(self,message):
         url = 'https://translate.yandex.net/api/v1.5/tr.json/translate?' 
         key = 'trnsl.1.1.20200420T153840Z.c64aa64b1ef12707.339289160fc33c01cc0c66ce2604c7200714bf48' 
         langs = ['ru-en','ru-de' ,'ru-tt' ,'ru-uk']
         langs_name = ['на английский', 'на немецкий', 'на татарский', 'на украинский']
-        message_revers = self.revers(message.text)
+        message_revers = message.text[::-1]
         endword = 0
         count = 0
         indicator = False
@@ -45,7 +48,8 @@ class Tel_bot:
                 indicator = True
             endword += 1
         endword -= 1
-        text_lang = self.revers(message_revers[:endword])
+        temp = message_revers[:endword]
+        text_lang = temp[::-1] 
         ir = 0
         lang = ''
         ind = False
@@ -58,12 +62,13 @@ class Tel_bot:
                 lang = langs[ir]
                 break
             ir += 1 
-
         if ind == False:
             text = message.text[9:(len(message.text) - endword)] 
             reque = requests.get(url, data={'key': key, 'text': text, 'lang': lang})
             translated = reque.json()
             self.sendMes(message, translated["text"])
+
+#Я знаю что можно написать list[::-1]
     def revers(self, text):
         message_revers = ''
         i = len(text)
@@ -71,6 +76,7 @@ class Tel_bot:
             i -= 1 
             message_revers += text[i]
         return message_revers
+    
     def log(self, message):
         history = open('history.txt', 'a')
         print('Message {1} from {0}\n\n-----------------------------\n'.format(message.from_user.first_name, message.text))
